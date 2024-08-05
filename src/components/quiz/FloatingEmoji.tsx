f90251f7287cffddb0f568ecf6b3c36394904e32
lime <joohr1234@gmail.com>
2024-08-05 17:46:34 +0900

    feat: 퀴즈 화면 생성

---
+++ src/components/quiz/FloatingEmoji.tsx
@@ -0,0 +1,40 @@
+// FloatingEmoji.tsx
+//효림이가 만든 하트가 나오는 버튼!
+import React, { useState } from 'react';
+import './floatingHeart.css';
+
+// Emoji 타입 정의
+interface Emoji {
+  id: number;
+  emoji: string;
+}
+
+const FloatingEmoji: React.FC = () => {
+  const [emojis, setEmojis] = useState<Emoji[]>([]);
+
+  const addEmoji = () => {
+    const newEmoji: Emoji = {
+      id: Math.random(),
+      emoji: '💖',
+    };
+    setEmojis([...emojis, newEmoji]);
+
+    // 일정 시간 후 이모지를 제거
+    setTimeout(() => {
+      setEmojis((emojis) => emojis.filter((e) => e.id !== newEmoji.id));
+    }, 2000); // 애니메이션 시간이 2초이므로, 2초 후에 제거
+  };
+
+  return (
+    <div style={{ position: 'relative', height: '100px' }}>
+      <button onClick={addEmoji}>Emoji Up!</button>
+      {emojis.map((emoji) => (
+        <span key={emoji.id} className="emoji">
+          {emoji.emoji}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default FloatingEmoji;
